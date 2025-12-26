f76aa7d60ff22b7e17761e89f174ae08cc206671
yeongmin1231 <152240297+yeongmin1231@users.noreply.github.com>
2025-12-26 18:21:54 +0900

    Delete comufarm directory

--- comufarm/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-export default function Logo({ mode }) {
-  return (
-    <div className="logo">
-      {mode === "company" && <h1>comufarm for company</h1>}
-      {mode === "farmer" && <h1>comufarm for farmer</h1>}
-      {mode === "neutral" && <h1>comufarm</h1>}
-    </div>
-  );
-}
